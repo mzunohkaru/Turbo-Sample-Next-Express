4dfe917867d7ec74b944d01ddcb7737e0646466d
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-09-26 00:55:39 +0900

    feat:generate token func

---
+++ apps/server/src/lib/token.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+
+import { TokenPayload } from '@repo/schema'
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret_key'
+
+export function generateToken(payload: TokenPayload): string {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
+}
+
+export function verifyToken(token: string): object | string {
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    return 'トークンが無効です';
+  }
+}
