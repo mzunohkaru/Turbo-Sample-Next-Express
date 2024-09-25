f09d6c335e44f5215b9d5b623f79e28674f14b1f
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-09-26 01:01:29 +0900

    style:format

--- apps/server/src/lib/token.ts
+++ apps/server/src/lib/token.ts
@@ -5,13 +5,13 @@ import { TokenPayload } from '@repo/schema'
 const SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret_key'
 
 export function generateToken(payload: TokenPayload): string {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' })
 }
 
 export function verifyToken(token: string): object | string {
   try {
-    return jwt.verify(token, SECRET_KEY);
+    return jwt.verify(token, SECRET_KEY)
   } catch (error) {
-    return 'トークンが無効です';
+    return 'トークンが無効です'
   }
 }
