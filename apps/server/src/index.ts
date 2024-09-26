6876793684fd56b5809b004dd2c8ff5ffd0fca5b
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-09-27 00:04:48 +0900

    add:supabase package

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { createClient } from '@supabase/supabase-js'
 
 import authRouter from './router/auth'
 import userRouter from './router/user'
@@ -9,6 +10,11 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string
+)
 
 app.use(cors())
 app.use(express.json())
@@ -20,6 +26,45 @@ app.use('/', (req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!')
+})
+
+app.post('/auth/test/signup', async (req, res) => {
+  const { email, password } = req.body
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  })
+  if (error) {
+    res.status(400).json({ message: error.message })
+  }
+  res.json(data)
+})
+
+app.post('/auth/test/login', async (req, res) => {
+  const { email, password } = req.body
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+  if (error) {
+    return res.status(400).json({ message: error.message })
+  }
+  res.json(data)
+})
+
+app.post('/auth/test/otp', async (req, res) => {
+  const { email } = req.body
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {
+      shouldCreateUser: true,
+      emailRedirectTo: process.env.CLIENT_URL,
+    },
+  })
+  if (error) {
+    res.status(400).json({ message: error.message })
+  }
+  res.json(data)
 })
 
 app.use('/auth', authRouter)
